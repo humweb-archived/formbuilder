d1b3dcab2bdf603f137a9022bc5675814b4075e0
Ryan Shofner <ryan@letsgolearn.com>
2015-08-15 14:24:48 -0700

    add generic text field

---
+++ src/Fields/TextField.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Humweb\FormBuilder\Fields;
+
+class TextField extends AbstractField
+{
+
+    /**
+     * Render the field
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $this->setAttribute('type', 'text');
+        $this->setAttribute('value', $this->value);
+        $this->setAttribute('name', $this->name);
+
+        return '<input '.$this->renderAttributes().' />';
+    }
+
+
+    /**
+     * Render the value
+     *
+     * @return string
+     */
+    public function renderValue()
+    {
+        return $this->value;
+    }
+}
